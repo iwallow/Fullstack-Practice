4a4630715db2f354fc464f5dbd56fa47098d39ef
iwallow <iwallow0@gmail.com>
2022-08-17 07:46:28 +0900

    complete part3-a [simple web server]

---
+++ index.js
@@ -0,0 +1,37 @@
+// require('http') 如同 import http from 'http' 的意思 
+// 利用CommandJS模块来导入包
+const http = require('http')
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2022-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2022-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2022-05-30T19:20:14.298Z",
+      important: true
+    }
+]
+
+// 调用http.createServer()来创建新的网络服务器
+// 内部为处理函数
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  // JSON.stringify()方法是将数组转化为JSON格式
+  response.end(JSON.stringify(notes))
+})
+
+// 给app变量绑定监听的端口3001
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
