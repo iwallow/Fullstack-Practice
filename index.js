8a143e32355b2951c8c16322cfa99ef9c2e9b8ae
iwallow <iwallow0@gmail.com>
2022-08-22 09:23:14 +0900

    update part3-a

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// require('http') 如同 import http from 'http' 的意思 
-// 利用CommandJS模块来导入包
-const http = require('http')
-
 let notes = [
     {
       id: 1,
@@ -23,15 +19,83 @@ let notes = [
     }
 ]
 
-// 调用http.createServer()来创建新的网络服务器
-// 内部为处理函数
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  // JSON.stringify()方法是将数组转化为JSON格式
-  response.end(JSON.stringify(notes))
+const express = require('express')
+const app = express() 
+
+// 用来解析JSON数据的中间件，如果不配置该功能的话，无法获取body中的JSON数据
+// ➡️是因为body中的数据是JSON格式，而不是JavaScript对象
+app.use(express.json())
+
+// 响应的content-type和状态码会被自动设置
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
 })
 
-// 给app变量绑定监听的端口3001
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.get('/api/notes', (request, response) => {
+    // 通过向json()方法传入字符串的形式，使响应的content-type自动被设置为application/json
+    // ※ JSON.stringify()返回的是字符串格式的JSON
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    
+    //id和note.id的类型不同，因此需要类型转换
+    const id = Number(request.params.id)
+
+    // 数组的方法find和filter的区别
+    // find只会返回对应的元素
+    // filter会返回包含对应元素的数组
+    const note = notes.find( note => note.id === id )
+
+    // 利用条件语句，在id错误时返回404
+    if(note){
+        response.json(note)
+    }else{
+        response.status(404).end()
+    }
+    
+})
+
+// 删除内容不返回任何数据，状态码为204
+app.delete('/api/notes/:id',(request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter( note => note.id !== id)
+    response.status(204).end()
+})
+
+const generateID = () => {
+    // ...的意思是展开的意思，通过展开语法给max()传递参数
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    // console.log(request.get('user-agent'))
+    // console.log(request.headers)
+    // console.log(request.body)
+
+    const body = request.body
+   
+    if(!body.content){
+        return response.status(400).json({
+            error : "content missing"
+        })
+    }
+
+    console.log(body.important)
+    const note = {
+        content : body.content,
+        important : body.important || false, 
+        date : new Date(), 
+        id : generateID()
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
+const PORT = '3001'
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
